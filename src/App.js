e66dca4e9d1220e4b29c0b5b2b6e864e509a98d5
Radik Abubjazov <abubjazov@mail.ru>
2021-10-08 17:58:05 +0300

    React Hooks: создание кастомных Hook'ов

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
-import Main from './code_samples/Main'
-import Alert from './code_samples/Alert/Alert'
-import { AlertProvider } from './code_samples/Alert/AlertContext'
+
+
+// function App() {  
+//   const [name, setName] = useState(''),
+//         [lastName, setLastName] = useState('')
+
+//   const changeNameHandler = e => setName(e.target.value),
+//         changeLastNameHandler = e => setLastName(e.target.value)
+
+//   return (
+//     <div className="App">
+//       <input tupe='text' value={name} onChange={changeNameHandler}/>
+//       <input tupe='text' value={lastName} onChange={changeLastNameHandler}/>
+//       <hr />
+//       <h1>{name} {lastName}</h1>
+//     </div>
+//   )
+// }
+
+function useLogger(value) {
+  useEffect(() => {
+    console.log('Value changed: ', value)
+  }, [value])
+}
+
+function useInput(initValue) {
+  const [value, setValue] = useState(initValue),
+        onChange = e => setValue(e.target.value),
+        clear = () => setValue('')
+
+  return {
+    bind: {value, onChange},
+    value, 
+    clear
+  }
+}
 
 function App() {  
+  const input = useInput(''),
+        lastName = useInput('')
+
+  useLogger(input.value)
+  useLogger(lastName.value)
+
   return (
-    <AlertProvider>
-      <div className="App">
-        <Alert />
-        <Main />
-      </div>
-      </AlertProvider>
+    <div className="App">
+      <input tupe='text' {...input.bind}/>
+      <input tupe='text' {...lastName.bind}/>
+      <hr />
+      <h1>{input.value} {lastName.value}</h1>
+      <button onClick={() => {
+        input.clear() 
+        lastName.clear()
+        }}>Clear</button>
+    </div>
   )
 }
 
