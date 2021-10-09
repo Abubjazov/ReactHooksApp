52b174ea3c40653eab4c34aeedb99d0396688180
Radik Abubjazov <abubjazov@mail.ru>
2021-10-09 19:58:07 +0300

    тема: React Transition Group

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react'
+import { Transition } from 'react-transition-group'
 
 function App() {  
-  const [toggle, setToggle] = useState(true)
+  const [toggle, setToggle] = useState(false)
 
   return (
     <div className='container'>
       <button onClick={() => setToggle(!toggle)}>Toggle</button>
       <hr/>
-      <div className='blocks'>
-      {toggle && <div className='square blue'>{toggle.toString()}</div>}
-        {/* {toggle ? <div className='square blue'>{toggle.toString()}</div> : null} */}
+      <div className={'blocks'}>
+        <Transition 
+          in={toggle} 
+          timeout={{
+            enter: 500,
+            exit: 300
+          }}
+          mountOnEnter
+          unmountOnExit
+        >
+          {state => (
+            <div className={`square blue ${state}`}>{state}</div>
+          )}
+        </Transition>
       </div>      
     </div>
   )
