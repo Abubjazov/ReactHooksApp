4e5f5eae27bb30d700b82198d8e6e1c1e6d039e5
Radik Abubjazov <abubjazov@mail.ru>
2021-10-09 19:26:23 +0300

    тема: добавление CSS анимаций в React

--- src/App.js
+++ src/App.js
@@ -1,59 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-
-
-// function App() {  
-//   const [name, setName] = useState(''),
-//         [lastName, setLastName] = useState('')
-
-//   const changeNameHandler = e => setName(e.target.value),
-//         changeLastNameHandler = e => setLastName(e.target.value)
-
-//   return (
-//     <div className="App">
-//       <input tupe='text' value={name} onChange={changeNameHandler}/>
-//       <input tupe='text' value={lastName} onChange={changeLastNameHandler}/>
-//       <hr />
-//       <h1>{name} {lastName}</h1>
-//     </div>
-//   )
-// }
-
-function useLogger(value) {
-  useEffect(() => {
-    console.log('Value changed: ', value)
-  }, [value])
-}
-
-function useInput(initValue) {
-  const [value, setValue] = useState(initValue),
-        onChange = e => setValue(e.target.value),
-        clear = () => setValue('')
-
-  return {
-    bind: {value, onChange},
-    value, 
-    clear
-  }
-}
+import React, { useState } from 'react'
 
 function App() {  
-  const input = useInput(''),
-        lastName = useInput('')
-
-  useLogger(input.value)
-  useLogger(lastName.value)
+  const [toggle, setToggle] = useState(true)
 
   return (
-    <div className="App">
-      <input tupe='text' {...input.bind}/>
-      <input tupe='text' {...lastName.bind}/>
-      <hr />
-      <h1>{input.value} {lastName.value}</h1>
-      <button onClick={() => {
-        input.clear() 
-        lastName.clear()
-        }}>Clear</button>
+    <div className='container'>
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      <hr/>
+      <div className='blocks'>
+      {toggle && <div className='square blue'>{toggle.toString()}</div>}
+        {/* {toggle ? <div className='square blue'>{toggle.toString()}</div> : null} */}
+      </div>      
     </div>
   )
 }
