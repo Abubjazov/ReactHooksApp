625fee528314a20063acf69ed6b2c86715568b91
Radik Abubjazov <abubjazov@mail.ru>
2021-10-09 21:04:30 +0300

    тема: анимация групп элементов

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react'
 import { Transition, CSSTransition } from 'react-transition-group'
+import { List } from './List'
 
 function App() {  
   const [toggle, setToggle] = useState(false)
   const [toggle2, setToggle2] = useState(false)
+  const [items, setItems] = useState([
+    {id: 1, title: 'Item 1'},
+    {id: 2, title: 'Item 2'},
+    {id: 3, title: 'Item 3'},
+    {id: 4, title: 'Item 4'},
+  ])
+
+  const removeItem = id => setItems(items.filter(i => i.id !== id))
+
+  const addItem = () => {
+    const title = prompt('Enter item title')
+    const id = Date.now()
+
+    setItems(items.concat([{id, title}]))
+  }
 
   return (
     <div className='container'>
       <button onClick={() => setToggle(!toggle)}>Toggle</button>
       <button onClick={() => setToggle2(!toggle2)}>Toggle2</button>
+      <button onClick={addItem}>Add Item</button>
       <hr/>
       <div className={'blocks'}>
         <Transition 
@@ -42,7 +59,9 @@ function App() {
             {toggle2.toString()}
           </div>
         </CSSTransition>
-      </div>      
+      </div>  
+
+      <List items={items} onRemove={removeItem}/>    
     </div>
   )
 }
